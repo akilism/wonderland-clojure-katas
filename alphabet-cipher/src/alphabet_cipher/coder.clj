f862d72d3ddd7e3676247859753dd4b249c90dce
akil harris <akilism@chillchillchillchillchillchill.com>
2015-06-13 20:18:02 -0400

    finished alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,59 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def alpha "abcdefghijklmnopqrstuvwxyz")
 
+;(get-char-idx \m alpha)
+(defn get-char-idx [c alpha-list]
+  (loop [n 0 x alpha-list]
+    (cond
+      (empty? x) 0
+      (= c (first x)) n
+      :else (recur (inc n) (rest x)))))
+
+;(get-char 10 alpha)
+(defn get-char [n xs]
+  (cond
+    (= n 0) (first xs)
+    :else (recur (dec n) (rest xs))))
+
+;(get-adjusted-alpha \m alpha)
+(defn get-adjusted-alpha [start-char alpha-list]
+    (loop [n (get-char-idx start-char alpha-list) acc []]
+      (cond
+        (= (count acc) (count alpha-list)) acc
+        (> n 25) (recur 0 acc)
+        :else (recur (inc n) (conj acc (get-char n alpha-list))))))
+
+;(get-encode-char \m \r alpha)
+(defn get-encode-char [key-char msg-char alpha-list]
+  (let [key-idx (get-char-idx key-char alpha-list) msg-idx (get-char-idx msg-char alpha-list) char-idx (+ key-idx msg-idx)]
+    (cond
+      (> char-idx 25) (get-char (- char-idx 26) alpha-list)
+      :else (get-char char-idx alpha-list))))
+
+;(get-decode-char \m \d alpha)
+(defn get-decode-char [key-char msg-char a-list]
+  (let [k-idx (get-char-idx key-char a-list)]
+    (loop [letters a-list alpha-list (get-adjusted-alpha (first letters) a-list) decode-char (first letters)]
+      (cond
+        (= msg-char (get-char k-idx alpha-list)) decode-char
+        :else (recur
+                (rest letters)
+                (get-adjusted-alpha (first (rest letters)) a-list)
+                (first (rest letters)))))))
+
+;(encode "hotdog" "doesthiswork")
+(defn encode [keyword message]
+  (loop [ks keyword ms message encoded []]
+    (cond
+      (empty? ms) (clojure.string/join encoded)
+      (empty? ks) (recur keyword ms encoded)
+      :else  (recur (rest ks) (rest ms) (conj encoded (get-encode-char (first ks) (first ms) alpha))))))
+
+;(decode "hotdog" "kcxvhnpgprfq")
 (defn decode [keyword message]
-  "decodeme")
+  (loop [ks keyword ms message decoded []]
+    (cond
+      (empty? ms) (clojure.string/join decoded)
+      (empty? ks) (recur keyword ms decoded)
+      :else (recur (rest ks) (rest ms) (conj decoded (get-decode-char (first ks) (first ms) alpha))))))
--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,22 @@
 (ns wonderland-number.finder)
+;; You must find a way to generate this wonderland number.
+
+;; It has six digits
+;; If you multiply it by 2,3,4,5, or 6, the resulting number
+;; has all the same digits in at as the original number.
+;; The only difference is the position that they are in.
+(defn same-digits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
+(defn is-wonderland? [wonder-number]
+  (if (and
+        (same-digits? wonder-number (* wonder-number 2))
+        (same-digits? wonder-number (* wonder-number 3))
+        (same-digits? wonder-number (* wonder-number 4))
+        (same-digits? wonder-number (* wonder-number 5))
+        (same-digits? wonder-number (* wonder-number 6))) true false))
 
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter is-wonderland? (range 100000 999999))))
